b32e50ff3e65e92ab9ff1c8c70b0566c17a501ab
DESKTOP-LEPLK0Q\jimmy <jimmy778878@gmail.com>
2022-03-05 06:57:17 +0800

    update

--- src/test.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-a = np.array([2,1,0])
-print("a: ", a)
-print("softmax result: ",np.exp(-1*a) / np.sum(np.exp(-1*a)))
-
-b = a - np.average(a)
-print("b: ", b)
-print("softmax result: ", np.exp(-1*b) / np.sum(np.exp(-1*b)))
-
