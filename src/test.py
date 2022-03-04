30a5cd0e7a654699ebefe735316a989fdf01d5c2
DESKTOP-LEPLK0Q\jimmy <jimmy778878@gmail.com>
2022-03-05 06:41:47 +0800

    update

--- src/test.py
+++ src/test.py
@@ -1,9 +1,9 @@
 import numpy as np
-import torch
-from util.minimum_edit_distance import minimum_edit_distance
+a = np.array([2,1,0])
+print("a: ", a)
+print("softmax result: ",np.exp(-1*a) / np.sum(np.exp(-1*a)))
 
-a = np.array([100, 110, 150])
-print(np.exp(a) / np.sum(np.exp(a)))
 b = a - np.average(a)
-b = np.exp(b)
-print(b / np.sum(b))
+print("b: ", b)
+print("softmax result: ", np.exp(-1*b) / np.sum(np.exp(-1*b)))
+
