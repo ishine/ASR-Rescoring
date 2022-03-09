039a9b89bdabba0471dc671e6a1d697eb86b4d1e
DESKTOP-LEPLK0Q\jimmy <jimmy778878@gmail.com>
2022-03-09 15:44:22 +0800

    update

--- src/mwer_training.py
+++ src/mwer_training.py
@@ -22,31 +22,36 @@ class MWERTraining():
             file_path=file_path,
             requirements=["hyp_text", "hyp_score", "hyp_cer"],
             max_utts=self.config.max_utts,
+            n_best=self.config.n_best,
             flatten=False
         )
         
         input_ids = []
         attention_masks = []
         for hyps in tqdm(hyp_texts, total=len(hyp_texts)):
+            
             batch_ids = []
             batch_attention_masks = []
+            
             for hyp in hyps:
-
                 word_pieces = self.tokenizer.tokenize(hyp)
 
-                if len(word_pieces) < self.config.max_seq_len:
-                    batch_ids.append(
-                        self.tokenizer.convert_tokens_to_ids(
-                            ["[CLS]"] + word_pieces + ["[SEP]"]
-                        )
+                if len(word_pieces) > self.config.max_seq_len:
+                    break
+                
+                batch_ids.append(
+                    self.tokenizer.convert_tokens_to_ids(
+                        ["[CLS]"] + word_pieces + ["[SEP]"]
                     )
+                )
 
-                    batch_attention_masks.append(
-                        [1] * len(["[CLS]"] + word_pieces + ["[SEP]"])
-                    )
+                batch_attention_masks.append(
+                    [1] * len(["[CLS]"] + word_pieces + ["[SEP]"])
+                )
 
-            input_ids.append(batch_ids)
-            attention_masks.append(batch_attention_masks)
+            if len(batch_ids) == self.config.n_best:
+                input_ids.append(batch_ids)
+                attention_masks.append(batch_attention_masks)
 
         dataset = self.MyDataset(input_ids, attention_masks, asr_scores, hyp_cers)
         
@@ -56,18 +61,28 @@ class MWERTraining():
         dataloader = DataLoader(
             dataset=dataset,
             collate_fn=self.collate,
-            batch_size=1,
+            batch_size=self.config.batch_size,
         )
         return dataloader
 
     def collate(self, data):
-        input_ids_tensor, attention_masks_tensor, asr_scores_tensor, hyp_cer_tensor = data[0]
+        input_ids_tensor_sets, attention_masks_tensor_sets, \
+            asr_scores_tensor_sets, hyp_cer_tensor_sets = zip(*data)
 
         batch = {}
-        batch["input_ids_tensor"] = pad_sequence(input_ids_tensor, batch_first=True)
-        batch["attention_masks_tensor"] = pad_sequence(attention_masks_tensor, batch_first=True)
-        batch["asr_scores_tensor"] = asr_scores_tensor
-        batch["hyp_cer_tensor"] = hyp_cer_tensor
+
+        batch["input_ids"] = []
+        for input_ids_tensor_set in input_ids_tensor_sets:
+            batch["input_ids"] += input_ids_tensor_set
+        batch["input_ids"] = pad_sequence(batch["input_ids"], batch_first=True)
+
+        batch["attention_masks"] = []
+        for attention_masks_tensor_set in attention_masks_tensor_sets:
+            batch["attention_masks"] += attention_masks_tensor_set
+        batch["attention_masks"] = pad_sequence(batch["attention_masks"], batch_first=True)
+        
+        batch["asr_scores"] = torch.stack(asr_scores_tensor_sets)
+        batch["hyp_cer"] = torch.stack(hyp_cer_tensor_sets)
 
         return batch
 
@@ -108,18 +123,20 @@ class MWERTraining():
         epoch_loss = 0
         loop = tqdm(enumerate(dataloader), total=len(dataloader))
         for _, batch in loop:
-            input_ids_tensor = batch["input_ids_tensor"].to(self.config.device)
-            attention_masks_tensor = batch["attention_masks_tensor"].to(self.config.device)
-            asr_scores_tensor = batch["asr_scores_tensor"].to(self.config.device)
-            hyp_cer_tensor = batch["hyp_cer_tensor"].to(self.config.device)
+            input_ids = batch["input_ids"].to(self.config.device)
+            attention_masks = batch["attention_masks"].to(self.config.device)
+            asr_scores = batch["asr_scores"].to(self.config.device)
+            hyp_cer = batch["hyp_cer"].to(self.config.device)
 
             with torch.set_grad_enabled(train_mode):
-                LM_scores_tensor = self.model(
-                    input_ids=input_ids_tensor,
-                    attention_mask=attention_masks_tensor
+                LM_scores = self.model(
+                    input_ids=input_ids,
+                    attention_mask=attention_masks
                 ).squeeze(dim=1)
-                
-                batch_loss = self.compute_loss(LM_scores_tensor, asr_scores_tensor, hyp_cer_tensor)
+
+                LM_scores = torch.reshape(LM_scores, (self.config.batch_size, self.config.n_best))
+
+                batch_loss = self.compute_loss(asr_scores, LM_scores, hyp_cer)
 
                 if train_mode:
                     batch_loss.backward()
@@ -130,14 +147,15 @@ class MWERTraining():
 
         return epoch_loss / len(dataloader)
 
-    def compute_loss(self, LM_scores_tensor, asr_scores_tensor, hyp_cer_tensor):
-        final_scores = asr_scores_tensor + LM_scores_tensor
+    def compute_loss(self, asr_scores, LM_scores, hyp_cer):
+        final_scores = asr_scores + LM_scores
         
-        probility = torch.softmax(-1*final_scores, dim=0)
-        
-        average_cer = torch.sum(hyp_cer_tensor) / len(hyp_cer_tensor)
+        probility = torch.softmax(-1*final_scores, dim=1)
 
-        batch_loss = torch.dot(probility, (hyp_cer_tensor - average_cer))
+        average_cer = torch.sum(hyp_cer,dim=1) / self.config.n_best
+        average_cer = average_cer.unsqueeze(dim=1)
+
+        batch_loss = torch.sum(torch.mul(probility, (hyp_cer - average_cer)))
 
         return batch_loss
 
