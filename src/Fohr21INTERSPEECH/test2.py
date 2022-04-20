38d9b5e88fec63275a724b8ddb8a91b0c0d370b0
DESKTOP-LEPLK0Q\jimmy <jimmy778878@gmail.com>
2022-04-20 09:48:49 +0800

    update

--- src/Fohr21INTERSPEECH/test2.py
+++
@@ -1,4 +0,0 @@
-from transformers import BertModel
-
-bert = BertModel.from_pretrained("bert-base-chinese", attention_probs_dropout_prob=0.3, hidden_dropout_prob=0.3)
-print(bert.config)
--- src/preprocessing/oracle.py
+++ src/preprocessing/oracle.py
@@ -1,5 +1,6 @@
 import argparse
 import json
+import matplotlib.pyplot as plt
 from jiwer import cer
 from parse_json import parse_json
 
@@ -16,11 +17,31 @@ if __name__ == "__main__":
         requirements=["ref_text", "hyp_text", "hyp_cer"],
     )
     
+    n_best = len(hypotheses[0])
+    utt_num = len(hypotheses)
+
+    oracle_count = {pos: 0 for pos in range(n_best)}
     hyp = []
     for utt_hypotheses, utt_hyp_cers in zip(hypotheses, hypotheses_cers):
         min_value = min(utt_hyp_cers)
+        min_value_pos = utt_hyp_cers.index(min_value)
+        oracle_count[min_value_pos] += 1
         min_id = utt_hyp_cers.index(min_value)
         hyp.append(utt_hypotheses[min_id])
     
 
     print(cer(ref, hyp))
+
+    print(oracle_count)
+    oracle_dictribution = [num/utt_num for num in oracle_count.values()]
+    print(oracle_dictribution)
+    plt.hist(
+        oracle_count.values(),
+        bins=n_best,
+        density=True,
+        color = 'lightblue',
+        cumulative = False,
+        label = "Height"
+    )
+    plt.ylabel("probility")
+    plt.show()
