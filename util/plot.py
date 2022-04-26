f545c1c34ee1b55edee7bf29e72cc228fdc97c93
chkuo <jimmy778878@gmail.com>
2022-04-26 11:30:51 +0800

    update

--- util/plot.py
+++ util/plot.py
@@ -1,4 +1,5 @@
 from typing import Dict, List
+import json
 import matplotlib.pyplot as plt
 
 def plot(out_file: str, curve_names: List, curves: List, labels: Dict):
@@ -13,4 +14,15 @@ def plot(out_file: str, curve_names: List, curves: List, labels: Dict):
     plt.savefig(out_file)
 
 if __name__ == "__main__":
-    plot()
+    loss_json = json.load(
+        open("../RescoreBert/result/MD_MWED/loss.json", "r", encoding="utf-8")
+    )
+    train_loss = loss_json["train"]
+    dev_loss = loss_json["dev"]
+
+    plot(
+        out_file="../RescoreBert/result/MD_MWED/loss",
+        curve_names=["train", "dev"],
+        curves=[train_loss, dev_loss],
+        labels={"y": "loss","x": "epoch"}
+    )
