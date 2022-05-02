85f190054c6d4154ccf57df5ed372d5beb39c48d
chkuo <jimmy778878@gmail.com>
2022-05-02 16:19:11 +0800

    update

--- CorrectBart/compute_cer.py
+++ CorrectBart/compute_cer.py
@@ -1,7 +1,7 @@
 from jiwer import cer
 import json
 ref_file = "/home/chkuo/chkuo/experiment/ASR-Rescoring/espnet_data/alfred/test/ref_text.json"
-pred_file = "/home/chkuo/chkuo/experiment/ASR-Rescoring/CorrectBart/result/one_hyp/dev_pred.json"
+pred_file = "/home/chkuo/chkuo/experiment/ASR-Rescoring/CorrectBart/result/one_hyp/test_pred.json"
 
 ref_json = json.load(open(ref_file, "r", encoding="utf-8"))
 pred_json = json.load(open(pred_file, "r", encoding="utf-8"))
